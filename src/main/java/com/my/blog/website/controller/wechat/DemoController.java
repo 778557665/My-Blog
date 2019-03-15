8e0495b73911af1f6a400c6bc5a0b5db3c1bcdd9
翁周俊 <wengzhoujun@zhibitech.com>
2019-03-15 11:56:33 +0800

    demo接口修改

--- src/main/java/com/my/blog/website/controller/wechat/DemoController.java
+++ src/main/java/com/my/blog/website/controller/wechat/DemoController.java
@@ -1,9 +1,11 @@
 package com.my.blog.website.controller.wechat;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/wechat")
+@RestController
+@RequestMapping("/wechat")
 public class DemoController {
 
     @GetMapping("/demo")
